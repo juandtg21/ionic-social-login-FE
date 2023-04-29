27277901246df4fac914bb11bc153c627f9c6d3f
juandtg21 <juandtg21@gmail.com>
2023-04-29 11:48:28 -0500

    update appId and scroll behavior in login and registration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.greet.app',
   appName: 'greet',
   webDir: 'www',
   bundledWebRuntime: false
